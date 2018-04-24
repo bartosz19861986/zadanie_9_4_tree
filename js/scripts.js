a7de106bf178089d0b55b4e96291dfa09439bd8e
Bartosz Pluta <barpluta@o2.pl>
2018-04-24 15:19:47 +0200

    Add JS

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+ function drawTree(k) {
+	var i;
+	var j;
+	var k;
+	
+	for ( i = 0 ; i < 1 ; i++) {
+		var star="";
+				for ( j = 0 ; j < k ; j++) {
+					star += '*';
+					console.log(star);
+				}
+	}
+
+} 
+
+		var tree1 = drawTree(10);
